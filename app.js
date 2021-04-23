7214a4cc284d251ec3ae5d26511c46f8d6a43668
IOINITID <50173108+IOINITID@users.noreply.github.com>
2021-04-23 23:20:34 +0300

    Added express-graphql and graphql

--- app.js
+++ app.js
@@ -1,14 +1,42 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const { graphqlHTTP } = require("express-graphql");
+const { buildSchema } = require("graphql");
 
 const app = express();
 const PORT = 3000;
 
 app.use(bodyParser.json());
 
-app.get("/", (req, res, next) => {
-  res.send("Server response.");
-});
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: buildSchema(`
+    type RootQuery {
+        events: [String!]!
+    }
+
+    type RootMutation {
+        createEvent(name: String): String
+    }
+
+    schema {
+        query: RootQuery
+        mutation:RootMutation
+    }
+    `),
+    rootValue: {
+      events: () => {
+        return ["Romantic Cooking", "Sailing", "All-Night Cooding"];
+      },
+      createEvent: (args) => {
+        const eventName = args.name;
+        return eventName;
+      },
+    },
+    graphiql: true,
+  })
+);
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}...`);
