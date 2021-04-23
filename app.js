f46aa4711059731805d5fbd649254b4bc8c08086
IOINITID <50173108+IOINITID@users.noreply.github.com>
2021-04-23 22:48:08 +0300

    Added get route

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const PORT = 3000;
 
 app.use(bodyParser.json());
 
+app.get("/", (req, res, next) => {
+  res.send("Server response.");
+});
+
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}...`);
 });
