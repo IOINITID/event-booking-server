1eb506eea82349270174a0b00185af7f2352d40c
IOINITID <50173108+IOINITID@users.noreply.github.com>
2021-04-27 22:11:56 +0300

    Made code refactoring

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -4,6 +4,15 @@ const Event = require("../../models/event");
 const User = require("../../models/user");
 const Booking = require("../../models/booking");
 
+const transformEvent = (event) => {
+  return {
+    ...event._doc,
+    _id: event.id,
+    date: new Date(event._doc.date).toISOString(),
+    creator: user.bind(this, event.creator),
+  };
+};
+
 const events = async (eventIds) => {
   try {
     const events = await Event.find({ _id: { $in: eventIds } });
