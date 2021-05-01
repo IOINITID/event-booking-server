a664735223d781953c7c8260cac6484217915a6d
IOINITID <50173108+IOINITID@users.noreply.github.com>
2021-05-01 14:03:40 +0300

    Added json web token

--- graphql/resolvers/auth.js
+++ graphql/resolvers/auth.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require("../../models/user");
 
 module.exports = {
@@ -20,6 +21,31 @@ module.exports = {
       const result = await user.save();
 
       return { ...result._doc, _id: result.id, password: null };
+    } catch (error) {
+      throw error;
+    }
+  },
+  login: async ({ email, password }) => {
+    try {
+      const user = await User.findOne({ email: email });
+
+      if (!user) {
+        throw new Error("User does not exist!");
+      }
+
+      const isEqual = await bcrypt.compare(password, user.password);
+
+      if (!isEqual) {
+        throw new Error("Password is incorrect!");
+      }
+
+      const token = jwt.sign(
+        { userId: user.id, email: user.email },
+        "somesupersecretkey",
+        { expiresIn: "1h" }
+      );
+
+      return { userId: user.id, token: token, tokenExpiration: 1 };
     } catch (error) {
       throw error;
     }
