242ca24143ce9d6ab2dbb0e746bef2904b5f3505
IOINITID <50173108+IOINITID@users.noreply.github.com>
2021-08-19 22:57:16 +0300

    Updated package.json file

--- index.js
+++ index.js
@@ -30,21 +30,6 @@ const startApolloServer = async () => {
 
   await server.start();
 
-  // app.use((req, res, next) => {
-  //   res.setHeader("Access-Control-Allow-Origin", "*");
-  //   res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
-  //   res.setHeader(
-  //     "Access-Control-Allow-Headers",
-  //     "Content-Type, Authorization"
-  //   );
-
-  //   if (req.method === "OPTIONS") {
-  //     return res.sendStatus(200);
-  //   }
-
-  //   next();
-  // });
-
   app.use(cors());
 
   app.use(express.json({ limit: "50mb" }));
