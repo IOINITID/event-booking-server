48a37f463d2907fb0044a315bac7d5bb8d635cbe
IOINITID <50173108+IOINITID@users.noreply.github.com>
2021-08-20 08:38:54 +0300

    Updated env variables

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 
-if (process.env.MODE !== "production") {
+if (process.env.MODE === "development") {
   dotenv.config();
 }
 
